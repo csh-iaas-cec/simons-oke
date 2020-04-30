7252599f41ff2c92f8ecdfa1b89fbebf2f668f6a
csh-iaas-cec <51996734+csh-iaas-cec@users.noreply.github.com>
2020-04-30 23:52:23 +0530

    updates NSG

--- oke/adb.tf
+++ oke/adb.tf
@@ -33,7 +33,7 @@ resource "oci_database_autonomous_database" "autonomous_database" {
   license_model                                  = "${var.autonomous_database_license_model}"
   is_preview_version_with_service_terms_accepted = "false"
   subnet_id                                      = oci_core_subnet.s-w1.id
-  nsg_ids                                        = [oci_core_network_security_group_security_rule.egress_network_security_group_security_rule.id,oci_core_network_security_group_security_rule.egress_network_security_group_security_rule.id]
+  nsg_ids                                        = [oci_core_network_security_group.simmons_network_security_group]
 }
 
 resource "oci_database_autonomous_database_backup" "autonomous_database_backup" {
